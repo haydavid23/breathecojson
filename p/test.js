9a3f5b305b281894ae1edbc1454743e1cb0b7c52
David Hay <hay.david@hotmail.com>
2019-09-09 01:24:15 +0000

    update

--- p/test.js
+++ p/test.js
@@ -12,7 +12,7 @@ const server = https.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  //console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 
